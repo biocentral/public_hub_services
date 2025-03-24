4e7d999897bb1d1062b3d8de5c8a408f79646aff
Sebastian <sebastian.franz@tum.de>
2025-03-24 12:20:13 +0100

    Refactoring leaderboard entry handling

--- public_hub_services/plm_leaderboard/plm_leaderboard_database.py
+++ public_hub_services/plm_leaderboard/plm_leaderboard_database.py
@@ -11,13 +11,16 @@ logger = logging.getLogger(__name__)
 
 
 class PLMLeaderboardDatabase:
-    _metadata_keys = ['model_name', 'dataset_name', 'split_name', 'biotrainer_version', 'training_date']
+    key_delimiter = '\t'
 
     def __init__(self, backup_data: Optional[Path] = None):
         redis_host = os.environ.get('REDIS_HOST', 'localhost')
-        redis_port = int(os.environ.get('REDIS_PORT', 6379))
+        redis_port = int(os.environ.get('REDIS_PORT', 6380))
 
         self.redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+        print(self.redis_client.info())
+        self._clear_database()
 
         if backup_data is not None:
             self._clear_database()
@@ -30,56 +33,43 @@ class PLMLeaderboardDatabase:
             raise ValueError("Unsupported file format. Use .csv, .yml, or .yaml")
 
     def add_entry(self, entry: Dict[str, Dict[str, Any]]) -> bool:
-        # Separate metadata and metrics
-        metadata = entry["metadata"]
-        metrics = entry["metrics"]
-
         # Store metadata and metrics in a hash
-        entry_id = f"{metadata['model_name']}:{metadata['dataset_name']}:{metadata['split_name']}"
-        self.redis_client.hset(f"plm-leaderboard:{entry_id}", mapping={
-            'metadata': json.dumps(metadata),
-            'metrics': json.dumps(metrics)
-        })
+        entry_id = f"{entry['modelName']}{self.key_delimiter}{entry['trainingDate']}"
+        self.redis_client.set(f"plm-leaderboard:{entry_id}", json.dumps(entry))
 
         # Add to set of all entries
         self.redis_client.sadd('plm-leaderboard:all_entries', entry_id)
 
         return True
 
-    def add_publishing_data(self, results: List[Dict[str, Any]]) -> bool:
-        # TODO Verification
-        for entry in results:
-            self.add_entry(entry)
+    def add_publishing_data(self, result: Dict[str, Any]) -> bool:
+        self.add_entry(result)
 
         # TODO Remove, only debug
         self.export_to_yaml(output_path=Path("published.yml"))
         return True
 
-    def get_entry(self, model_name: str, dataset_name: str, split_name: str) -> Optional[Dict[str, any]]:
-        entry_id = f"{model_name}:{dataset_name}:{split_name}"
-        entry = self.redis_client.hgetall(f"plm-leaderboard:{entry_id}")
+    def get_entry(self, entry_id: str) -> Optional[Dict[str, any]]:
+        entry = self.redis_client.get(f"plm-leaderboard:{entry_id}")
         if not entry:
             return None
 
-        metadata = json.loads(entry['metadata'])
-        metrics = json.loads(entry['metrics'])
-        return {"metadata": metadata, "metrics": metrics}
+        return entry
 
-    def get_all_data(self) -> Dict[str, Dict[str, any]]:
+    def get_all_data(self) -> List[Dict[str, any]]:
         all_entry_ids = self.redis_client.smembers('plm-leaderboard:all_entries')
-        return {str(idx): self.get_entry(*entry_id.split(':')) for idx, entry_id in enumerate(all_entry_ids)}
+        return [self.get_entry(entry_id) for entry_id in all_entry_ids]
 
-    def update_entry(self, entry: Dict[str, any]) -> bool:
-        return self.add_entry(entry)  # add_entry will overwrite if the key already exists
-
-    def delete_entry(self, model_name: str, dataset_name: str, split_name: str) -> bool:
-        entry_id = f"{model_name}:{dataset_name}:{split_name}"
+    """
+    def delete_entry(self, model_name: str) -> bool:
+        entry_id = f"{model_name}"
         key = f"plm-leaderboard:{entry_id}"
         if self.redis_client.exists(key):
             self.redis_client.delete(key)
             self.redis_client.srem('plm-leaderboard:all_entries', entry_id)
             return True
         return False
+    """
 
     def _clear_database(self) -> bool:
         try:
@@ -93,11 +83,13 @@ class PLMLeaderboardDatabase:
 
     def export_to_yaml(self, output_path: Path):
         data = self.get_all_data()
+        export = {'leaderboard': data}
         with open(output_path, 'w') as file:
-            yaml.dump(data, file)
+            yaml.dump(export, file)
 
     def import_from_yaml(self, input_path: Path):
         with open(input_path, 'r') as file:
             data = yaml.safe_load(file)
-        for entry in data.values():
-            self.add_entry(entry)
+        for entry in data['leaderboard']:
+            entry_dict = json.loads(entry)
+            self.add_entry(entry_dict)
--- public_hub_services/plm_leaderboard/plm_leaderboard_endpoint.py
+++ public_hub_services/plm_leaderboard/plm_leaderboard_endpoint.py
@@ -1,7 +1,7 @@
 import json
 
 from .plm_leaderboard_database import PLMLeaderboardDatabase
-from .plm_leaderboard_ranking import calculate_ranking, get_recommended_metrics
+from .plm_leaderboard_ranking import get_recommended_metrics
 
 from flask import Blueprint, jsonify, current_app, request
 
@@ -9,24 +9,22 @@ plm_leaderboard_service_route = Blueprint('plm_leaderboard', __name__)
 
 def _get_leaderboard_dict(plm_leaderboard_db):
     all_data = plm_leaderboard_db.get_all_data()
-    ranking = calculate_ranking(all_data.values())
     recommended_metrics = get_recommended_metrics()
-    return {"ranking": ranking, "leaderboard": all_data, "recommended_metrics": recommended_metrics}
+    return {"leaderboard": all_data, "recommended_metrics": recommended_metrics}
 
 @plm_leaderboard_service_route.route('/plm_leaderboard/', methods=['GET'])
 def plm_leaderboard():
     plm_leaderboard_db: PLMLeaderboardDatabase = current_app.config["PLM_LEADERBOARD_DATABASE"]
     return jsonify(_get_leaderboard_dict(plm_leaderboard_db))
 
-
 @plm_leaderboard_service_route.route('/plm_leaderboard_publish/', methods=['POST'])
 def plm_leaderboard_publish():
     publish_data = request.get_json()
-    results = json.loads(publish_data["results"])
+    result = json.loads(publish_data["result"])
 
     plm_leaderboard_db: PLMLeaderboardDatabase = current_app.config["PLM_LEADERBOARD_DATABASE"]
 
-    if plm_leaderboard_db.add_publishing_data(results=results):
+    if plm_leaderboard_db.add_publishing_data(result=result):
         return jsonify(_get_leaderboard_dict(plm_leaderboard_db))
 
     # TODO
